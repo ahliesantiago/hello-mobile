cbbb6fc7ba937506b4dce4729615ba1ddc426e51
Ahlie Santiago <83854486+ahliesantiago@users.noreply.github.com>
2026-02-19 02:26:06 +0800

    feat: add dynamic name input and greeting

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,49 @@
 import { useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+} from "react-native";
 
 export default function Index() {
-  const [clicked, setClicked] = useState(false)
+  const [submitted, setSubmitted] = useState(false);
+  const [name, setName] = useState("");
+
+  function onChangeText(input) {
+    setName(input);
+    setSubmitted(false); // Reset submitted state when input changes
+  }
+
+  function handleSubmit() {
+    if (!submitted) {
+      // Capitalize first letter and submit
+      setName(name.charAt(0).toUpperCase() + name.slice(1));
+      setSubmitted(true);
+    } else {
+      // Reset
+      setName("");
+      setSubmitted(false);
+    }
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.homeText}>Hello{clicked ? ", World" : ""}!</Text>
+      <Text style={styles.homeText}>Hello{submitted ? `, ${name}` : ""}!</Text>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputLabel}>What's your name?</Text>
+        <TextInput
+          onChangeText={onChangeText}
+          value={name}
+          style={styles.input}
+        />
+      </View>
       <Button
-        onPress={() => setClicked(!clicked)}
-        title="Click me!"
+        onPress={handleSubmit}
+        title={submitted ? "Reset" : "Submit"}
         color="#CA7669"
-        accessibilityLabel="Button for 'Click Me'"
+        accessibilityLabel="Name submission button"
       />
     </View>
   );
@@ -27,5 +59,23 @@ const styles = StyleSheet.create({
   homeText: {
     fontWeight: "bold",
     fontSize: 40,
-  }
+  },
+  inputContainer: {
+    padding: 20,
+    width: "75%",
+  },
+  inputLabel: {
+    fontWeight: "semibold",
+    fontSize: 20,
+    textAlign: "center",
+  },
+  input: {
+    borderWidth: 1,
+    margin: 12,
+    paddingTop: 2,
+    paddingBottom: 2,
+    paddingHorizontal: 5,
+    fontSize: 15,
+    color: "black",
+  },
 })
