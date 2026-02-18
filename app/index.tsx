0dbc3e2b22d2d799690f9c9ac153075b5a8b0fbd
Ahlie Santiago <83854486+ahliesantiago@users.noreply.github.com>
2026-02-19 01:02:05 +0800

    feat: basic message and button

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,31 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const [clicked, setClicked] = useState(false)
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.homeText}>Hello{clicked ? ", World" : ""}!</Text>
+      <Button
+        onPress={() => setClicked(!clicked)}
+        title="Click me!"
+        color="#CA7669"
+        accessibilityLabel="Button for 'Click Me'"
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 20,
+  },
+  homeText: {
+    fontWeight: "bold",
+    fontSize: 40,
+  }
+})
